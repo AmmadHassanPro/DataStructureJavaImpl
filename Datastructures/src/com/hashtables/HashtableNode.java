4638794455444624ab38245783db6abdabd86a9e
ammad <ammad@10.0.0.92>
2019-04-10 21:56:16 -0500

    Added Hashtable Node in package

--- Datastructures/src/com/hashtables/HashtableNode.java
+++ Datastructures/src/com/hashtables/HashtableNode.java
@@ -3,6 +3,27 @@ package com.hashtables;
 // This will contain the key and value associated with it and a reference to the next element if any
 public class HashtableNode {
 	
+	private String key;
+	private int value; // assuming value will be in integer
+	private HashtableNode next;
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public HashtableNode getNext() {
+		return next;
+	}
+	public void setNext(HashtableNode next) {
+		this.next = next;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
 	
-//
+	
 }
