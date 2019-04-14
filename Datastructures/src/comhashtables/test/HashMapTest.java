267117e4471dbe23b3a8f1a6b653aae1aa03c33e
ammadhassan <ammadhassan@10.0.0.178>
2019-04-14 18:09:54 -0500

    fixed test cases, Junit impl coming soon

--- Datastructures/src/comhashtables/test/HashMapTest.java
+++ Datastructures/src/comhashtables/test/HashMapTest.java
@@ -7,7 +7,7 @@ public class HashMapTest {
 	
 	public static void main(String[] args) {
 		
-		HashMap testHashMap = new HashMap(10);
+		HashMap testHashMap = new HashMap(10); // by default the load factor will be 0.75
 		
 		testHashMap.put("one",1);
 		testHashMap.put("two",2);
@@ -16,19 +16,32 @@ public class HashMapTest {
 		testHashMap.put("five",5);
 		testHashMap.put("six",6);
 		testHashMap.put("seven",7);
+		//Size Will grow size=8,(can be confirmed from debugger)
 		testHashMap.put("eight",8);
 		
-		System.out.println("Getting Node 1");
-		HashtableNode result = testHashMap.get("one");
+		System.out.println("Getting Node 8");
+		HashtableNode result = testHashMap.get("eight");
 		System.out.println("Node Key:"+result.getKey()+" Value:"+result.getValue());
 		
-		
-		testHashMap.remove("one");
-		result = testHashMap.get("one");
+		// Size will shrink, size=7 (can be confirmed from debugger)
+		testHashMap.remove("eight");
+		result = testHashMap.get("eight");
 		if(result==null) {
 			System.out.println("Since node with key:one was removed, so we got null");
 			
 		}
+
+		
+		System.out.println("Resizing should happen now, removeing more now");
+		
+		testHashMap.remove("seven");
+		testHashMap.remove("six");
+		testHashMap.remove("five");
+		// Size will shrink,size=3 (can be confirmed from debugger)
+		testHashMap.remove("four");
+		
+		
+		
 		
 	}
 
