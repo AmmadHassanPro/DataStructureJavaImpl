eba761b4225798e29b4549eae4a5849cf540a7fc
ammadhassan <ammadhassan@10.0.0.178>
2019-04-17 20:26:34 -0500

    created a binary tree

---
+++ Datastructures/src/main/java/com/nodes/TreeNode.java
@@ -0,0 +1,62 @@
+package com.nodes;
+
+/*
+ * This class represent a node on a Binary Tree
+ */
+public class TreeNode{
+	
+	private TreeNode leftchild;
+	private TreeNode rightchild;
+	private int value;
+	private int childcount;
+	
+	public TreeNode(){
+		leftchild =null;
+		rightchild = null;
+		value =0;
+		childcount=0;
+	}
+
+	public TreeNode getLeftchild() {
+		childcount++;
+		return leftchild;
+		
+	}
+
+	public void setLeftchild(TreeNode leftchild) {
+		this.leftchild = leftchild;
+	}
+
+	public TreeNode getRighchild() {
+		return rightchild;
+	}
+
+	public void setRighchild(TreeNode righchild) {
+		childcount++;
+		this.rightchild = righchild;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	public void removeLeft() {
+		childcount--;
+		this.leftchild = null;
+	}
+	public void removeRight() {
+		childcount--;
+		this.rightchild = null;
+	}
+	
+	
+	
+	
+	
+	
+
+}
