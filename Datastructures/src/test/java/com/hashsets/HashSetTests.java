47f4c39c20dbd8069f516a9c49dff9659e62eb2a
ammadhassan <ammadhassan@10.0.0.178>
2019-06-23 14:55:39 -0500

    Added test case to check duplicates

--- Datastructures/src/test/java/com/hashsets/HashSetTests.java
+++ Datastructures/src/test/java/com/hashsets/HashSetTests.java
@@ -43,10 +43,27 @@ public class HashSetTests {
 		
 		assertTrue(objectUnderTest.remove(node4));
 		assertEquals(3,objectUnderTest.getCount());
+	}
+	
+	@Test
+	public void testDuplicates(){
+		objectUnderTest  = new HashSet();
+		Node node1 = new Node("Test Node 1", 1);
+		Node node2 = new Node("Test Node 2", 2);
+		Node node3 = new Node("Test Node 3", 3);
+		Node node4 = new Node("Test Node 4", 4);
+		Node duplicateNode = node4;
 		
 		
+		objectUnderTest.add(node1);
+		objectUnderTest.add(node2);
+		objectUnderTest.add(node3);
+		objectUnderTest.add(node4);
+		objectUnderTest.add(duplicateNode);
 		
+		assertEquals(4,objectUnderTest.getCount());
 		
 	}
+	
 
 }
